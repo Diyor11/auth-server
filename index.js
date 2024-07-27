012a45c9c228d46e28608e664c303f2e2bb11d32
Diyor11 <diyorq3arshi@gmail.com>
2024-07-27 18:49:52 +0500

    fixed cors error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json())
 
 const users = [
